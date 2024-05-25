875ffae53954525ee08867990a313ee76776527d
Vinicius Souza Fernando <vinicius.sfernando@hotmail.com>
2024-05-24 22:44:13 -0300

    Adicionar arquivos de projeto.

---
+++ Benchmarker/Demo.cs
@@ -0,0 +1,20 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace Benchmarker
+{
+    [MemoryDiagnoser]
+    public class Demo
+    {
+        [Benchmark]
+        public string GetFullName()
+        {
+            string output = "";
+            for (int i = 0; i < 1000; i++)
+            {
+                 output += i;
+            }
+
+            return output;
+        }
+    }
+}
---
+++ Benchmarker/Program.cs
@@ -0,0 +1,5 @@
+﻿using BenchmarkDotNet.Running;
+using Benchmarker;
+
+
+var results = BenchmarkRunner.Run<Demo>();
